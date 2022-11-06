3528363dde316218c73ff4092476682e4708edf0
Lu6ov <114359281+Lu6ov@users.noreply.github.com>
2022-11-06 18:08:52 +0300

    Add files via upload
    занятие 2

---
+++ lesson2.js
@@ -0,0 +1,29 @@
+//const name = "Alex";
+
+//const nameToNumber = Number(name);
+
+//console.log(nameToNumber);
+
+//const age = 26;
+
+//const ageToString = String(age);
+
+//console.log(typeof ageToString);
+
+//console.log(typeof +"45");
+//console.log(typeof Number("45"));
+
+//console.log("2" + 2 + 3);
+
+//console.log(5 > 4);
+//console.log("ананас" > "яблоко");
+//console.log("2" > "12");
+//console.log(undefined == null);
+//console.log(undefined === null);
+//console.log(null == "\n0\n");
+//console.log(null === +"\n0\n");
+//console.log(""+ 1 + 0);
+//console.log("" - 1 + 0);
+//console.log(true + false);
+//console.log(6/"3");
+console.log(" -9 " - 5);
